f17e38c59597a5fded9ea77f2c929fcd68b52e35
Frédéric Meynadier <frederic.meynadier@bipm.org>
2019-07-23 13:55:24 +0000

    fix #99

--- tests/functional_tests/test_noise.py
+++ tests/functional_tests/test_noise.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python
 
+from allantools import noise
+import pytest # NOQA
 import sys
 sys.path.append("..")
-
-from allantools import noise
-import numpy
-import pytest
 
 
 def test_noise():
 
     N = 500
-    #rate = 1.0
-    w = noise.white(N)
-    b = noise.brown(N)
-    v = noise.violet(N)
-    p = noise.pink(N)
-    
+    w = noise.white(num_points=N)
+    b = noise.brown(num_points=N)
+    v = noise.violet(num_points=N)
+    p = noise.pink(num_points=N)
+
+    # Check that length of sample is as expected
     assert len(w) == N
     assert len(b) == N
     assert len(v) == N
     assert len(p) == N
-    
+
+
 if __name__ == "__main__":
     test_noise()
